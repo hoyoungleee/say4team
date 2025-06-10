aeb45c46ecee6dc8c3d9c9b055acb85c56de881f
hoyeong-i <ghdud939@gmail.com>
2025-06-10 11:42:40 +0900

    카테고리 관리 1차 커밋

---
+++ product-service/src/main/java/com/playdata/productservice/category/entity/Category.java
@@ -0,0 +1,40 @@
+package com.playdata.productservice.category.entity;
+
+import com.playdata.productservice.category.dto.CategoryResDto;
+import com.playdata.productservice.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_review")
+public class Category extends BaseTimeEntity {
+
+    //카테고리 아이디
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false)
+    private Long categoryId;
+
+    //카테고리 이름
+    @Column(name = "category_name",nullable = false)
+    private String categoryName;
+
+    //카테고리 화면 뒷배경 쓸 이미지 주소
+    @Column(length = 1000,name = "category_bg_ImgUrl", nullable = false)
+    private String categoryBgImgUrl;
+
+    public static CategoryResDto fromEntity(Category category) {
+        return CategoryResDto.builder()
+                .categoryId(category.categoryId)
+                .categoryName(category.categoryName)
+                .categoryBgImgUrl(category.categoryBgImgUrl)
+                .build();
+    }
+}
