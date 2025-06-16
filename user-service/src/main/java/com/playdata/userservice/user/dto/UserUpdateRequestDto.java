2ffeb0af2b682e42e50ad2b4e40c8fa76aec7add
KangWonBeen <WonKong01@naver.com>
2025-06-16 16:37:31 +0900

    회원정보 생년월일 수정 시 DB 오류 수정

--- user-service/src/main/java/com/playdata/userservice/user/dto/UserUpdateRequestDto.java
+++ user-service/src/main/java/com/playdata/userservice/user/dto/UserUpdateRequestDto.java
@@ -3,6 +3,8 @@ package com.playdata.userservice.user.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class UserUpdateRequestDto {
@@ -12,5 +14,6 @@ public class UserUpdateRequestDto {
     private String phone;
     private String email;
     private String password;
+    private LocalDate birthDate;
 
 }
